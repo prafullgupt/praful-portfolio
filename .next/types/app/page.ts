120da952dd6128223317a922ebc6c00769f1c427
prafullgupt <prafulgupta@Prafuls-MacBook-Air.local>
2026-09-08 21:23:42 +0530

    google add implement

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/prafulgupta/Documents/Project/praful-portfolio/app/page.tsx
+// File: /Users/prafulgupta/Documents/project/praful-portfolio/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { experienceStartYear } from "@/lib/data"
 import { ThemeProvider } from "@/app/context/ThemeContext"
 import AnalyticsProvider from "@/app/components/AnalyticsProvider" // <-- Add this
 import { MetaInfo, StructuredData } from "@/lib/MetaInfo"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,13 +23,20 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         <ThemeProvider>
-           <StructuredData />
+          <StructuredData />
           <AnalyticsProvider> {/* <-- Wrap children */}
             {children}
           </AnalyticsProvider>
         </ThemeProvider>
         <Analytics mode="production" debug={false} />
         <SpeedInsights debug={false} />
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
       </body>
     </html>
   )
